a778b9a24ea5fb8a2681610379561fe51913684c
Faisal <falfariheedi@gmail.com>
2021-09-26 18:03:12 +0300

    removed comments

--- app/src/main/java/com/example/number_gameapp/MainActivity.kt
+++ app/src/main/java/com/example/number_gameapp/MainActivity.kt
@@ -22,51 +22,42 @@ class MainActivity : AppCompatActivity() {
         var qgue=(0..11).random()
         var limit=3
 
-        //dialog builder here
-        // first we create a variable to hold an AlertDialog builder
+
         val dialogBuilder = AlertDialog.Builder(this)
-        //////////////////////////////////
+
         val dialogBuilderwin = AlertDialog.Builder(this)
-        ////////////////////////////////
+
         val dialogBuilderlose = AlertDialog.Builder(this)
-        // then we set up the input
-//        val inp = Button(this)  not needed
-        ///////////////////////////////////////
-//        val inpw = Button(this) not needed
-        ////////////////////////////////////
-//        val inpl = Button(this) not needed
-        // here we set the message of our alert dialog
+
         dialogBuilder.setMessage("Enter numbers ONLY")
-            // positive button text and action
+
             .setPositiveButton("i understand", DialogInterface.OnClickListener {
                     dialog, id -> null })
-            // negative button text and action
+
             .setNegativeButton("NO", DialogInterface.OnClickListener {
                     dialog, id -> exitProcess(0) })
-        ////////////////////////////////
+
         dialogBuilderwin.setMessage("Congrats you got the correct answer do you want to play again? ")
             .setPositiveButton("play again",DialogInterface.OnClickListener {
                 dialog, id ->  input.clear(); limit=3 ;qgue=(0..11).random(); rv.adapter = RVAdapter(input) })
             .setNegativeButton("NO",DialogInterface.OnClickListener{
                 dialog, id -> exitProcess(0)})
+
         dialogBuilderlose.setMessage("do you want to play again? ")
             .setPositiveButton("play again",DialogInterface.OnClickListener{
                     dialog, id ->  input.clear(); limit=3 ;qgue=(0..11).random(); rv.adapter = RVAdapter(input) })
             .setNegativeButton("NO",DialogInterface.OnClickListener{
                     dialog ,id -> exitProcess(0)})
-        // create dialog box
+
         val nullinput = dialogBuilder.create()
         val wind=dialogBuilderwin.create()
         val lossd=dialogBuilderlose.create()
-        // set title for alert dialog box
+
         nullinput.setTitle("wrong input")
         wind.setTitle("you win")
         lossd.setTitle("you lost")
 
-        // add the button
-//        nullinput.setView(inp) not needed
-//        wind.setView(inpw)
-//        lossd.setView(inpl)
+
 
 
 
